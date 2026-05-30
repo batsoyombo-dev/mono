547872b482e3e7d8d50d0f501fb418c3fd3e1933
batsoyombo-dev <batsoyombo.kh@gmail.com>
2026-05-30 15:43:32 +0800

    add: change 🔥

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+
+import { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file";
+
+const { combine, timestamp, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => `${timestamp} [${level}]: ${message}`);
+
+const logger = createLogger({
+    level: process.env.LOG_LEVEL || "info",
+    format: combine(
+        timestamp(),
+        process.env.NODE_ENV !== "production" ? colorize() : format.uncolorize(),
+        logFormat
+    ),
+    transports: [new transports.Console()],
+    exitOnError: false,
+});
+
+export { Logger } from "winston";
+export { logger };
